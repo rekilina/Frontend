347a9ab81025253884aba3007adc509899b481e0
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-01 22:06:05 +0400

    back to multiple useState

--- my-react-app/src/components/NewExpense/ExpenseForm.js
+++ my-react-app/src/components/NewExpense/ExpenseForm.js
@@ -2,26 +2,18 @@ import React, { useState } from 'react';
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-	const [userInput, setUserInput] = useState({
-		enteredTitle: '',
-		enteredAmount: '',
-		enteredDate: ''
-	});
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
+	const [enteredDate, setEnteredDate] = useState('');
 
 	const titleChangeHandler = (event) => {
-		setUserInput((prevState) => {
-			return { ...prevState, enteredTitle: event.target.value }
-		});
+		setEnteredTitle(event.target.value);
 	};
 	const amountChangeHandler = (event) => {
-		setUserInput((prevState) => {
-			return { ...prevState, enteredAmount: event.target.value }
-		});
+		setEnteredAmount(event.target.value);
 	};
 	const dateChangeHandler = (event) => {
-		setUserInput((prevState) => {
-			return { ...prevState, enteredDate: event.target.value }
-		});
+		setEnteredDate(event.target.value);
 	};
 
 	return (
