83914ffad923a6270c882a54cc50e48e451b5823
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-08 10:44:21 +0400

    useEffect & cleanup

--- login-starting-page/src/App.js
+++ login-starting-page/src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loggingHandler = () => {
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }
+  // if we don't specify [] then function only runs once
+  useEffect(loggingHandler, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
