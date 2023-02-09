3c77b325f304aef11776314e3ed7f4ad834dcc68
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-09 11:58:47 +0400

    cart modal Portals

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,6 @@
 import styles from "./AvailableMeals.module.css"
 import Card from '../UI/Card';
-import MealItem from './MealItem'
+import MealItem from './MealItem/MealItem'
 
 const DUMMY_MEALS = [
 	{
--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import styles from "./MealItem.module.css"
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
 	const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,7 @@ const MealItem = (props) => {
 				<p className={styles.price}>{price}</p>
 			</div>
 			<div>
-				ss
+				<MealItemForm />
 			</div>
 		</li>
 	);
