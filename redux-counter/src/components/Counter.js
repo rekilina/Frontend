5ed5e4d76d3663d6826afb3515fc49d1aa709910
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-15 00:16:39 +0400

    how to use redux in react

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  }
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div className={classes.buttons}>
+        <button onClick={incrementHandler} type="button">Increment</button>
+        <button onClick={decrementHandler} type="button">Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
