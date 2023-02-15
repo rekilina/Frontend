5524f859b792948cfa670395da9664d8f4299c23
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-15 21:34:05 +0400

    notify and prevent sending data on first load

--- react-redux-demo-app/src/App.js
+++ react-redux-demo-app/src/App.js
@@ -1,26 +1,68 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './components/store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    fetch('https://react-http-tests-5c0a0-default-rtdb.firebaseio.com/cart.json',
-      {// override existing card with incoming data
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      });
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }));
+      const response = await fetch('https://react-http-tests-5c0a0-default-rtdb.firebaseio.com/cart.json',
+        {// override existing card with incoming data
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+      const responseData = await response.json();
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success...',
+        message: 'Sent cart data sucessfully.'
+      }));
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: `Sent cart data failed. ${error}`
+      }));
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
