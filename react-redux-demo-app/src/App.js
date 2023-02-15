abb021cb8e1261200efb5ef9ba28316f11861260
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-15 20:58:30 +0400

    sied effect logic in component APP

--- react-redux-demo-app/src/App.js
+++ react-redux-demo-app/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,6 +6,16 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-tests-5c0a0-default-rtdb.firebaseio.com/cart.json',
+      {// override existing card with incoming data
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+  }, [cart]);
+
   return (
     <Layout>
       {cartIsVisible && <Cart />}
