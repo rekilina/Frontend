8474881e7c3f3489dace51d92ac4565af6223851
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-16 15:43:12 +0400

    add routing pages

--- routing-demo/src/App.js
+++ routing-demo/src/App.js
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+// path is what we have after the domain
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/products',
+    element: <ProductsPage />
+  }
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ routing-demo/src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+	return <h1>My ProductsPage</h1>
+}
+
+export default ProductsPage;
