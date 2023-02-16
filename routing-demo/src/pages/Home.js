7c67df778241af462ea53394aa83be70d9399785
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-16 20:18:34 +0400

    useNavigate

--- routing-demo/src/pages/Home.js
+++ routing-demo/src/pages/Home.js
@@ -1,9 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 function HomePage() {
+	const navigate = useNavigate();
+
+	function navigateHandler() {
+		navigate('/products');
+	}
+
 	return <>
 		<h1>My HomePage</h1>
+		<p>
+			<button onClick={navigateHandler}>Navigate</button>
+		</p>
 	</>
 }
 
