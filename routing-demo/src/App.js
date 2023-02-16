4e8500b8b972aa0485b9f1a375b0859515f3ebd7
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-16 16:22:46 +0400

    adding error page

--- routing-demo/src/App.js
+++ routing-demo/src/App.js
@@ -2,6 +2,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom';
+import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
