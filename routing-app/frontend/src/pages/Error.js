9eb0a6d7698cbb402aa21d43f1175d0cada59303
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-17 12:11:21 +0400

    useRouteError

--- routing-app/frontend/src/pages/Error.js
+++ routing-app/frontend/src/pages/Error.js
@@ -1,7 +1,16 @@
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
+import PageContent from "./PageContent"
+
 function ErrorPage() {
-	return (<>
-		<h1>An error occured.</h1>
-	</>);
+	const error = useRouteError();
+	const title = JSON.parse(error.data).message;
+
+	return <>
+		<MainNavigation />
+		<PageContent title={title}> Status: {error.status} </PageContent >;
+	</>
 }
 
 export default ErrorPage;
--- routing-app/frontend/src/pages/EventsPage.js
+++ routing-app/frontend/src/pages/EventsPage.js
@@ -15,11 +15,14 @@ function EventsPage() {
 export default EventsPage;
 
 export async function eventsLoader() {
-	const response = await fetch('http://localhost:8080/eventss');
+	const response = await fetch('http://localhost:8080/events');
 
 	if (!response.ok) {
 		// return { isError: true, message: "Couldn't fetch events." }
-		throw new Error("Couldn't fetch events.");
+		throw new Response(
+			JSON.stringify({ message: "Couldn't fetch events." }),
+			{ status: 500 }
+		);
 	} else {
 		const resData = await response.json();
 		return resData;
