6844a1c0362b888313350fc9923004f615232e70
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-20 21:26:36 +0400

    printing error messages

--- auth-router-app/frontend/src/components/AuthForm.js
+++ auth-router-app/frontend/src/components/AuthForm.js
@@ -1,16 +1,23 @@
 
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useSearchParams, useActionData } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
   const [searchParams, setSearchParams] = useSearchParams();
   const isLogin = (searchParams.get('mode') === 'login');
+  const actionData = useActionData();
 
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {actionData && actionData.errors && <ul>
+          {Object.values(actionData.errors).map(err => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>}
+        {actionData && actionData.message && <p>{actionData.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
