6f986cf03e8dab4f704805b15485d4f1fb63f5f1
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-20 17:33:09 +0400

    added default values

--- auth-router-app/frontend/src/pages/NewEvent.js
+++ auth-router-app/frontend/src/pages/NewEvent.js
@@ -1,7 +1,12 @@
 import EventForm from '../components/EventForm';
 
 function NewEventPage() {
-  return <EventForm method="post" />;
+  return <EventForm method="post" event={{
+    title: "New Event Title",
+    image: "https://i.pinimg.com/736x/a5/d7/79/a5d77948a4e099dbc636cbc73fec3653.jpg",
+    date: "2023-01-03",
+    description: "New event description"
+  }} />;
 }
 
 export default NewEventPage;
