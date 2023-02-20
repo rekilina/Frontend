d7082e4736ea5aef0d08eded2604f33a2a786930
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-20 23:04:24 +0400

    automatic logout (one way)

--- auth-router-app/frontend/src/pages/Root.js
+++ auth-router-app/frontend/src/pages/Root.js
@@ -1,15 +1,27 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, 1 * 60 * 60 * 1000);
+
+  }, [token, submit]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
