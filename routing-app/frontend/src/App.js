0c943f11c11b118fe64f88167f547beae0f8d0f6
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-20 15:00:43 +0400

    useFetcher() hook

--- routing-app/frontend/src/App.js
+++ routing-app/frontend/src/App.js
@@ -1,25 +1,3 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import {
   createBrowserRouter,
   RouterProvider
@@ -35,6 +13,7 @@ import EventsLayout from './pages/EventsLayout'
 import ErrorPage from './pages/Error';
 import { action as manipulateEventAction } from './components/EventForm';
 import { action as deleteEventAction } from './pages/EventDetailPage';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 function App() {
   const router = createBrowserRouter([
@@ -71,9 +50,13 @@ function App() {
               element: <NewEventPage />,
               action: manipulateEventAction
             }
-
           ]
-        }
+        },
+        {
+          path: 'newsletter',
+          element: <NewsletterPage />,
+          action: newsletterAction,
+        },
       ]
     }
   ]);
