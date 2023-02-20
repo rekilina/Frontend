f522ef6f5e4634d233d3bbe62c65126801de1bdd
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-20 23:14:31 +0400

    auto logout (better way)

--- auth-router-app/frontend/src/pages/Logout.js
+++ auth-router-app/frontend/src/pages/Logout.js
@@ -3,5 +3,6 @@ import { redirect } from 'react-router-dom';
 export function action() {
 	console.log('lofout');
 	localStorage.removeItem('token');
+	localStorage.removeItem('expiration');
 	return redirect("/");
 }
