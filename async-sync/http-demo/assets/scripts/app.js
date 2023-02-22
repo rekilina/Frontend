365d639637c1457c4063713dce74759177b3a6cf
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-22 11:49:51 +0400

    post request

--- async-sync/http-demo/assets/scripts/app.js
+++ async-sync/http-demo/assets/scripts/app.js
@@ -3,18 +3,18 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('post-template');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
 	const promise = new Promise((resolve, reject) => {
 		// create XMLHttpRequest
 		const xhr = new XMLHttpRequest();
 
 		// initialize it
 		// xhr.open(method, URL, [async, user, password])
-		xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+		xhr.open(method, url);
 
 		// send request
 		// xhr.send([body])
-		xhr.send();
+		xhr.send(JSON.stringify(data));
 
 		// This will be called after the response is received
 		xhr.onload = function () {
@@ -40,6 +40,7 @@ function sendHttpRequest(method, url) {
 
 // 		);
 // }
+
 async function fetchPosts() {
 	const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
 	const listOfPosts = JSON.parse(responseData);
@@ -52,6 +53,20 @@ async function fetchPosts() {
 	}
 }
 
+async function createPost(title, content) {
+	const userId = Math.random();
+	const post = {
+		title: title,
+		body: content,
+		userId: userId
+	}
+
+	sendHttpRequest('POST',
+		'https://jsonplaceholder.typicode.com/posts', post);
+
+}
+
+createPost('DUMMY', 'A dummy post');
 fetchPosts();
 
 
