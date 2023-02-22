aadea0a87a0e3c61f256ed0408e46b326e3523ea
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-22 12:39:08 +0400

    fetch

--- async-sync/http-demo/assets/scripts/app_XML.js
+++ async-sync/http-demo/assets/scripts/app_XML.js
@@ -7,33 +7,56 @@ const fetchBtn = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-	// fetch returns a promise,
-	// which results in response, 
-	// which has .json() method
-	// which also returns a promise 
-	// which results in parsed JS object
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(data),
-		headers: {
-			"Content-Type": "applicaton/json"
+	const promise = new Promise((resolve, reject) => {
+		// create XMLHttpRequest
+		const xhr = new XMLHttpRequest();
+
+		// initialize it
+		// xhr.open(method, URL, [async, user, password])
+		xhr.open(method, url);
+
+		// send request
+		// xhr.send([body])
+		xhr.send(JSON.stringify(data));
+
+		// This will be called after the response is received
+		xhr.onload = function () {
+			if (xhr.status >= 200 && xhr.status < 300) {
+
+				resolve(xhr.response);
+			} else {
+				reject(new Error('Smth went wrong!'));
+			}
 		}
-	}).then(response => {
-		if (response.status >= 200 && response.status < 300) {
-			return response.json()
-		} else {
-			throw new Error('Smth went wrong on the server-side');
+
+		xhr.onerror = function () {
+			reject(new Error('Smth went wrong!'));
 		}
-	}).catch(err => {
-		console.log(err);
-		throw new Error('Smth went wrong (catch block)');
 	});
+	return promise;
 }
+
+// function fetchPosts() {
+// 	sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+// 		.then(
+// 			responseData => {
+// 				const listOfPosts = JSON.parse(responseData);
+
+// 				for (const post of listOfPosts) {
+// 					const postEl = document.importNode(postTemplate.content, true);
+// 					postEl.querySelector('h2').textContent = post.title.toUpperCase();
+// 					postEl.querySelector('p').textContent = post.body;
+// 					listElement.append(postEl);
+// 				}
+// 			}
+
+// 		);
+// }
 
 async function fetchPosts() {
 	try {
-		const responseData = await sendHttpRequest('GET', 'htps://jsonplaceholder.typicode.com/posts');
-		const listOfPosts = responseData;
+		const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+		const listOfPosts = JSON.parse(responseData);
 
 		for (const post of listOfPosts) {
 			const postEl = document.importNode(postTemplate.content, true);
