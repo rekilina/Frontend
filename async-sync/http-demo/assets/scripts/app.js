362f972460379596194fdcc970901ca23dc7b2de
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-22 11:38:02 +0400

    async function

--- async-sync/http-demo/assets/scripts/app.js
+++ async-sync/http-demo/assets/scripts/app.js
@@ -3,21 +3,46 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('post-template');
 
-// create XMLHttpRequest
-let xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+	const promise = new Promise((resolve, reject) => {
+		// create XMLHttpRequest
+		const xhr = new XMLHttpRequest();
 
-// initialize it
-// xhr.open(method, URL, [async, user, password])
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+		// initialize it
+		// xhr.open(method, URL, [async, user, password])
+		xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
-// send request
-// xhr.send([body])
-xhr.send();
+		// send request
+		// xhr.send([body])
+		xhr.send();
 
-// This will be called after the response is received
-xhr.onload = function () {
-	// console.log(JSON.parse(xhr.response));
-	const listOfPosts = JSON.parse(xhr.response);
+		// This will be called after the response is received
+		xhr.onload = function () {
+			resolve(xhr.response);
+		}
+	});
+	return promise;
+}
+
+// function fetchPosts() {
+// 	sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+// 		.then(
+// 			responseData => {
+// 				const listOfPosts = JSON.parse(responseData);
+
+// 				for (const post of listOfPosts) {
+// 					const postEl = document.importNode(postTemplate.content, true);
+// 					postEl.querySelector('h2').textContent = post.title.toUpperCase();
+// 					postEl.querySelector('p').textContent = post.body;
+// 					listElement.append(postEl);
+// 				}
+// 			}
+
+// 		);
+// }
+async function fetchPosts() {
+	const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+	const listOfPosts = JSON.parse(responseData);
 
 	for (const post of listOfPosts) {
 		const postEl = document.importNode(postTemplate.content, true);
@@ -27,5 +52,6 @@ xhr.onload = function () {
 	}
 }
 
+fetchPosts();
 
 
